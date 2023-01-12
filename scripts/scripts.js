20ea0a247e02e9ed5821e09cbf6d9a0d3aa47aeb
Francsy <francsyludens@gmail.com>
2023-01-12 22:23:01 +0100

    add/authentication firebase funciones y cambios en los archivos html

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,70 @@
+//inicio de fb
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNOjqmJ9fQwc_ReQubkeKrLmqvmaiqk48",
+  authDomain: "quizv2-5c21a.firebaseapp.com",
+  projectId: "quizv2-5c21a",
+  storageBucket: "quizv2-5c21a.appspot.com",
+  messagingSenderId: "1087403902810",
+  appId: "1:1087403902810:web:e58c18ac400a7c75c21563"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+//Listener
+firebase.auth().onAuthStateChanged((user) => user ? console.log(`Está en el sistema:${user.email} ${user.uid}`) : console.log("no hay usuarios en el sistema"))
+
+
+/* ----------------------------------FUNCIONES----------------------------------*/
+//Funciones de firebase:
+
+const loginUser = (email, password) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((credential) => {
+      let user = credential.user;
+      console.log(`${user.email} está en el sistema (ID: ${user.uid})`)
+      alert(`${user.email} está en el sistema (ID: ${user.uid})`)
+      console.log('USER', user)
+    })
+    .catch((error) => {
+      console.log(error.code);
+      console.log(error.message)
+    })
+}
+
+//Función para deslogearse:
+const logOut = () => {
+  let user = firebase.auth().currentUser;
+  firebase.auth().signOut()
+    .then(() => {
+      console.log(user.mail + 'is out')
+    })
+    .catch((error) => {
+      console.log('Error: ' + error)
+    });
+  location.reload();
+}
+
+//Función que crea nuevo usuario:
+const createUser = (email, password) => {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((credential) => {
+      let user = credential.user;
+      console.log(`${user.email} registrado con ID: ${user.uid}`)
+      alert(`registrado ${user.email} con ID ${user.uid}`)
+      db.collection('usuarios')
+        .add({
+          id: user.uid,
+          email: user.email
+        })
+        .then((userDoc) => console.log(`New user document with ID: ${user.uid}`))
+        .catch((error) => console.error("Error adding document: ", error))
+    })
+    .catch((error) => {
+      console.log("Error" + error.message)
+    });
+}
+
 //Funciones auxiliares:
 const addMessage = (texto, tiempo) => {
   const pintaMensaje = () => document.getElementById("mensaje").innerHTML += `<p>${texto}</p>`
@@ -123,6 +190,8 @@ const crearPreguntas = (arrayPreguntas) => {
   })
 }
 
+
+/* ----------------------------------CODIGO PARA LAS PAGINAS----------------------------------*/
 //Creación de clave/array en el localstorage en caso de que no exista:
 for (let i = 0; i <= localStorage.length; i++) {
   let key = localStorage.key(i);
@@ -131,6 +200,19 @@ for (let i = 0; i <= localStorage.length; i++) {
   } else {
     localStorage.setItem("memoryCard", JSON.stringify([]));
   }
+}
+
+if (document.title == '¡Bienvenido al Quiz!' || document.title == 'Tu resultado' ) {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        //Creación de botón logout en el navigator de index
+        document.querySelector('.logout').innerHTML = `<button class="getout">Log out</button>`;
+        document.querySelector('.getout').onclick = logOut;
+    } else {
+        document.querySelector('.logout').innerHTML = '';
+    }
+  });
+
 }
 
 if (document.title == 'Quiz') {
@@ -149,7 +231,7 @@ if (document.title == 'Tu resultado') {
 }
 
 if (document.title == '¡Bienvenido al Quiz!') {
-  
+
   let arrayGuardado = JSON.parse(localStorage.getItem("memoryCard"));
   //Comprobación de datos para la apertura del mensaje inicial
   if (arrayGuardado.length == 0) {
@@ -217,4 +299,38 @@ if (document.title == '¡Bienvenido al Quiz!') {
   addMessage('...por ahora ;)', 16000);
   addMessage('La puntuación no va a aparecer por mucho que mires esta pantalla', 30000);
   addMessage('¿Un minuto esperando? ¡Ánimate y pulsa!?', 60000);
+
+  //Login 
+
+  document.getElementById("formlog").addEventListener("submit", function (event) {
+    event.preventDefault();
+    let mail = event.target.elements.logemail.value;
+    let pass = event.target.elements.logpassword.value;
+    console.log(mail, pass)
+    loginUser(mail, pass)
+  })
+  //Apertura del registro de usuario
+  const botonRegistro = document.getElementById('openregistration');
+  const registro = document.getElementById('register');
+  botonRegistro.onclick = () => registro.style.display !== 'block' ? registro.style.display = 'block' : registro.style.display = 'none' 
+
+  //Registro de usuario
+  document.getElementById('formreg').addEventListener('submit', (event) => {
+    event.preventDefault();
+    let mail = event.target.elements.email.value;
+    let password = event.target.elements.password1.value;
+    let checkPass = event.target.elements.password2.value;
+    password === checkPass ? createUser(mail, password) : alert('Error: la contraseña debe ser la misma en los dos campos')
+  })
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        botonRegistro.style.display = 'none';
+        document.querySelector('#user-logged').style.display = 'block';
+        document.querySelector('#nouser').style.display = 'none';
+    } else {
+        document.querySelector('#register').style.display = 'none';
+        document.querySelector('#nouser').style.display = 'block';
+    }
+});
 }
